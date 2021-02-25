ad07fb7fd13c0a7ffbabcddd1b85fd5793e85935
Gustaf <gustafdelport@gmail.com>
2021-02-25 21:56:23 +0200

    Update Decorations.java

--- DataAccessLayer/Decorations.java
+++ DataAccessLayer/Decorations.java
@@ -5,9 +5,21 @@ import java.util.List;
 public class Decorations {
     private int decoID;
     private String decoName;
-    private int decoQant;
+    private int decoQuant;
 
     public Decorations(){}
+
+    public void setDecoID(int DecoID){
+        this.decoID = DecoID;
+    }
+
+    public void setDecoName(String DecoName){
+        this.decoName= DecoName;
+    }
+
+    public void setDecoQant(int DecoQuant){
+        this.decoQuant = DecoQuant;
+    }
 
     public List<Decorations> getDecorations(){
 
