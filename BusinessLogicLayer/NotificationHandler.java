d25833e1a0a0b7d73b71b57c1d120a36052771b4
Gustaf <gustafdelport@gmail.com>
2021-02-25 22:18:19 +0200

    Update NotificationHandler.java

--- BusinessLogicLayer/NotificationHandler.java
+++ BusinessLogicLayer/NotificationHandler.java
@@ -4,11 +4,19 @@ import DataAccessLayer.*;
 
 public class NotificationHandler {
 
-    //use the user
+    //use the user to get info and send mails and conformations
 
     public NotificationHandler(){}
 
     public void SendMail(user user){
 
     }
+
+    public void confirmBooking(user user){
+
+    }
+
+    public void genNotification(){
+        
+    } 
 }
