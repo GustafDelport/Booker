6ed998d36f7b15a6412215fa83715584d1cb509e
Gustaf Delport <gustafdelport@gmail.com>
2021-03-03 21:02:23 +0200

    Update user.java

--- DataAccessLayer/user.java
+++ DataAccessLayer/user.java
@@ -66,4 +66,12 @@ public class user {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int iD) {
+        ID = iD;
+    }
 }
