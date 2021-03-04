7b5b4cb0f2cd8dc8be1d5fa5b961371d67312d1f
Gustaf <gustafdelport@gmail.com>
2021-03-04 16:47:20 +0200

    Refactor
    LOL

---
+++ src/DataAccessLayer/LogisticsDataHandler.java
@@ -0,0 +1,28 @@
+package DataAccessLayer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LogisticsDataHandler {
+    
+    public LogisticsDataHandler(){
+        // Code here
+    }
+
+    public void GatherInformation(){
+        //Get User Questions
+    }
+
+    public List<Menu> getFoodOrder(ArrayList<String> ID){
+
+        // Temporary - used instead of "null"
+        return Collections.emptyList();
+    }
+
+    public List<Decorations> getDecorationsOrder(ArrayList<String> ID){
+        
+        // Temporary - used instead of "null"
+        return Collections.emptyList();
+    }
+}
---
+++ src/booker/Main.java
@@ -0,0 +1,11 @@
+package booker;
+
+import PresentationLayer.Home;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Home h = new Home();
+        h.init();
+    }
+}
