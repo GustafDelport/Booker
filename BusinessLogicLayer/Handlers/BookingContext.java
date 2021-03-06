0a8aac420476d0c86c35a64fa3f8f2ac121d83cd
Ruan Jansen van Vuuren <50712048+RuanJvv@users.noreply.github.com>
2021-03-06 08:40:31 +0200

    Layout Improvement

--- BusinessLogicLayer/Handlers/BookingContext.java
+++ BusinessLogicLayer/Handlers/BookingContext.java
@@ -2,20 +2,24 @@ package BusinessLogicLayer.Handlers;
 
 import DataAccessLayer.DataObjets.bookings;
 
-public class BookingContext {
+public class BookingContext 
+{
     private BookingInterface bInterface;
 
     public BookingContext(){}
 
-    public BookingContext(BookingInterface BInterface){
+    public BookingContext(BookingInterface BInterface)
+    {
         this.bInterface = BInterface;
     }
 
-    public void Execute(bookings booking){
+    public void Execute(bookings booking)
+    {
         bInterface.MakeBooking(booking);
     }
 
-    public void ExecuteTwo(bookings booking){
+    public void ExecuteTwo(bookings booking)
+    {
         bInterface.EditPrice(booking);
     }
 
