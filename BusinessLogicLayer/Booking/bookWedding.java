19d87371ea4507086f8e1ad9a89253f839fbfeee
Ruan Jansen van Vuuren <50712048+RuanJvv@users.noreply.github.com>
2021-03-06 08:35:28 +0200

    Layout Improvement

--- BusinessLogicLayer/Booking/bookWedding.java
+++ BusinessLogicLayer/Booking/bookWedding.java
@@ -6,18 +6,21 @@ import java.util.Random;
 import BusinessLogicLayer.Handlers.*;
 import DataAccessLayer.DataObjets.bookings;
 
-public class bookWedding implements BookingInterface {
+public class bookWedding implements BookingInterface 
+{
     public String type = "Wedding";
     private int pricePerPerson = 125;
 
     @Override
-    public void MakeBooking(bookings booking) {
+    public void MakeBooking(bookings booking) 
+    {
         Random rnd = new Random();
 
         String bID = String.format("WDG%04d", rnd.nextInt(1000));
         booking.setBookingID(bID);
 
-        if (booking.getNumberOfPeople() >= 40) {
+        if (booking.getNumberOfPeople() >= 40) 
+        {
             booking.setPrice((pricePerPerson * booking.getNumberOfPeople())*0.85);
         }
         else booking.setPrice((pricePerPerson * booking.getNumberOfPeople()));
@@ -37,10 +40,12 @@ public class bookWedding implements BookingInterface {
     }
 
     @Override
-    public bookings EditPrice(bookings booking) {
+    public bookings EditPrice(bookings booking) 
+    {
         bookings b = booking;
         
-        if (booking.getNumberOfPeople() >= 40) {
+        if (booking.getNumberOfPeople() >= 40) 
+        {
             booking.setPrice((pricePerPerson * booking.getNumberOfPeople())*0.85);        
         }
         else booking.setPrice((pricePerPerson * booking.getNumberOfPeople()));
