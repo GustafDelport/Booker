2bd99bb851d07fba3f7be3793edb57d3fc675c0f
Ruan Jansen van Vuuren <50712048+RuanJvv@users.noreply.github.com>
2021-03-06 08:44:28 +0200

    Final Update

--- DataAccessLayer/DataObjets/Admin.java
+++ DataAccessLayer/DataObjets/Admin.java
@@ -6,26 +6,31 @@ public class Admin {
     private String name;
     private List<String> notifications;
 
-    public Admin(String name, List<String> notifications) {
+    public Admin(String name, List<String> notifications) 
+    {
         this.setName(name);
         this.setNotifications(notifications);
     }
 
     public Admin() {}
 
-    public List<String> getNotifications() {
+    public List<String> getNotifications() 
+    {
         return notifications;
     }
 
-    public void setNotifications(List<String> notifications) {
+    public void setNotifications(List<String> notifications)
+    {
         this.notifications = notifications;
     }
 
-    public String getName() {
-		return name;
+    public String getName() 
+    {
+	return name;
 	}
 
-	public void setName(String name) {
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
 }
